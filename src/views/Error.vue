beccdfab29cc9bec070f8117109c989f5102e15c
MathieuElisabeth <mathieuregina75@gmail.com>
2022-11-11 14:07:21 +0100

    (feat): Add 404 page

---
+++ src/views/Error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <img :src="errorImg" alt="" class="img404">
+      <p class="text-center mt-20">
+          <router-link to="/">Go to Home</router-link>
+      </p>
+  </div>
+</template>
+
+<script>
+import errorImg from "@/assets/svg/404.svg"
+export default {
+  data() {
+      return {
+        errorImg
+      }
+  }
+}
+</script>
+
+<style>
+.img404 {
+  width: 100%;
+}
+.text-center{
+    text-align: center;
+}
+.mt-20 {
+  margin-top: 20px;
+}
+.text-center a {
+  padding: 10px;
+  background: white;
+  border-radius: 5px;
+  font-weight: bold;
+}
+</style>
